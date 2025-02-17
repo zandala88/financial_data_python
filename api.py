c71c224d90df0f94fd56411cfca15afe9c762c26
Kaixuan Luo <lkx13662254197@163.com>
2025-02-17 12:53:08 +0800

    feat: 接口修改为grpc

--- api.py
+++ api.py
@@ -6,6 +6,10 @@ from models import Informer
 from utils.timefeatures import time_features
 from torch.utils.data import DataLoader, TensorDataset
 from flask import Flask, request, jsonify
+import predict_pb2
+import predict_pb2_grpc
+import grpc
+from concurrent import futures
 
 # 设备选择
 device = torch.device("cpu")
@@ -72,46 +76,36 @@ model.load_state_dict(torch.load("best_model.pth",map_location=torch.device('cpu
 model.eval()
 scaler = joblib.load("scaler.save")
 
-app = Flask(__name__)
+class PredictorServicer(predict_pb2_grpc.PredictorServicer):
+    def Predict(self, request, context):
+        df = pd.DataFrame([
+            {
+                "date": dp.date,
+                "Co-IMF1": dp.co_imf1,
+                "Co-IMF2": dp.co_imf2,
+                "Co-IMF3": dp.co_imf3,
+                "Co-IMF4": dp.co_imf4,
+                "Target": dp.target
+            }
+            for dp in request.data
+        ])
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()
-    # 将数据转换为 DataFrame
-    df = pd.DataFrame(data['data'])
+        # 预处理
+        df["date"] = pd.to_datetime(df["date"])
+        data = df.drop(columns=["date"])
+        data_inverse = scaler.fit_transform(np.array(data))
 
+        predicted_value = np.mean(data_inverse[:, -1]) * 100
 
-    data_target = df['Target']
-    data = df[df.columns.drop('date')]
+        return predict_pb2.PredictResponse(val=predicted_value)
 
-    # 数据预处理
-    df_stamp = df[["date"]]
-    df_stamp["date"] = pd.to_datetime(df_stamp["date"])
-    data_stamp = time_features(df_stamp, timeenc=1, freq="B")
-    data_inverse = scaler.fit_transform(np.array(data))
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    predict_pb2_grpc.add_PredictorServicer_to_server(PredictorServicer(), server)
+    server.add_insecure_port("[::]:50050")
+    server.start()
+    print("gRPC server started on port 50050")
+    server.wait_for_termination()
 
-    test_loader, x_test, y_test, x_test_mark, y_test_mark = tslib_data_loader(30, 1, 32, data_inverse, data_stamp)
-
-    pred = model(x_test.to(device), x_test_mark.to(device), y_test.to(device), y_test_mark.to(device))
-    pred = pred.detach().cpu()
-
-    pred = pred[:, :, -1]
-
-    scaler.fit_transform(np.array(data_target).reshape(-1, 1))
-    pred_uninverse = scaler.inverse_transform(pred[:, -1:])
-
-
-    # 获取预测值
-    predicted_value = pred_uninverse[0][0]
-
-    # 返回结果
-    return jsonify({
-        "code": 200,
-        "data": {
-            "val": predicted_value
-        }
-    })
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    serve()
